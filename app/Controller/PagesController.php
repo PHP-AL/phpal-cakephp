e9cd9fac12a4130b414d66f236d20cc6b5756f20
Juarez P. A. Filho <juarezpaf@gmail.com>
2012-05-29 09:36:52 -0300

    Trying to make Opauth works

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -36,21 +36,21 @@ class PagesController extends AppController {
  *
  * @var string
  */
-	public $name = 'Pages';
+  public $name = 'Pages';
 
 /**
  * Default helper
  *
  * @var array
  */
-	public $helpers = array('Html', 'Session');
+  public $helpers = array('Html', 'Session');
 
 /**
  * This controller does not use a model
  *
  * @var array
  */
-	public $uses = array();
+  public $uses = array();
 
 /**
  * Displays a view
@@ -58,25 +58,30 @@ class PagesController extends AppController {
  * @param mixed What page to display
  * @return void
  */
-	public function display() {
-		$path = func_get_args();
+  public function display() {
+    $path = func_get_args();
 
-		$count = count($path);
-		if (!$count) {
-			$this->redirect('/');
-		}
-		$page = $subpage = $title_for_layout = null;
+    $count = count($path);
+    if (!$count) {
+      $this->redirect('/');
+    }
+    $page = $subpage = $title_for_layout = null;
 
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
-		$this->render(implode('/', $path));
-	}
+    if (!empty($path[0])) {
+      $page = $path[0];
+    }
+    if (!empty($path[1])) {
+      $subpage = $path[1];
+    }
+    if (!empty($path[$count - 1])) {
+      $title_for_layout = Inflector::humanize($path[$count - 1]);
+    }
+    $this->set(compact('page', 'subpage', 'title_for_layout'));
+    $this->render(implode('/', $path));
+  }
+
+  public function displayinfo() {
+    $this->set('data', $this->data);
+  }
+
 }
---
+++ app/View/Pages/displayinfo.ctp
@@ -0,0 +1,2 @@
+<?php
+debug($this->data);
