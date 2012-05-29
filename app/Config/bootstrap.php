d7c3f915246ecf2ef7e691a70a61a6dc07af3479
Juarez P. A. Filho <juarezpaf@gmail.com>
2012-05-29 00:38:46 -0300

    Added the Opauth Cake Plugin

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * This file is loaded automatically by the app/webroot/index.php file after core.php
  *
- * This file should load/create any application wide configuration settings, such as 
+ * This file should load/create any application wide configuration settings, such as
  * Caching, Logging, loading additional configuration files.
  *
  * You should also use this file to include any files that provide global functions/constants
@@ -29,59 +29,59 @@
  *
  * File storage engine.
  *
- * 	 Cache::config('default', array(
- *		'engine' => 'File', //[required]
- *		'duration'=> 3600, //[optional]
- *		'probability'=> 100, //[optional]
- * 		'path' => CACHE, //[optional] use system tmp directory - remember to use absolute path
- * 		'prefix' => 'cake_', //[optional]  prefix every cache file with this string
- * 		'lock' => false, //[optional]  use file locking
- * 		'serialize' => true, // [optional]
- * 		'mask' => 0666, // [optional] permission mask to use when creating cache files
- *	));
+ *   Cache::config('default', array(
+ *    'engine' => 'File', //[required]
+ *    'duration'=> 3600, //[optional]
+ *    'probability'=> 100, //[optional]
+ *    'path' => CACHE, //[optional] use system tmp directory - remember to use absolute path
+ *    'prefix' => 'cake_', //[optional]  prefix every cache file with this string
+ *    'lock' => false, //[optional]  use file locking
+ *    'serialize' => true, // [optional]
+ *    'mask' => 0666, // [optional] permission mask to use when creating cache files
+ *  ));
  *
  * APC (http://pecl.php.net/package/APC)
  *
- * 	 Cache::config('default', array(
- *		'engine' => 'Apc', //[required]
- *		'duration'=> 3600, //[optional]
- *		'probability'=> 100, //[optional]
- * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
- *	));
+ *   Cache::config('default', array(
+ *    'engine' => 'Apc', //[required]
+ *    'duration'=> 3600, //[optional]
+ *    'probability'=> 100, //[optional]
+ *    'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+ *  ));
  *
  * Xcache (http://xcache.lighttpd.net/)
  *
- * 	 Cache::config('default', array(
- *		'engine' => 'Xcache', //[required]
- *		'duration'=> 3600, //[optional]
- *		'probability'=> 100, //[optional]
- *		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional] prefix every cache file with this string
- *		'user' => 'user', //user from xcache.admin.user settings
- *		'password' => 'password', //plaintext password (xcache.admin.pass)
- *	));
+ *   Cache::config('default', array(
+ *    'engine' => 'Xcache', //[required]
+ *    'duration'=> 3600, //[optional]
+ *    'probability'=> 100, //[optional]
+ *    'prefix' => Inflector::slug(APP_DIR) . '_', //[optional] prefix every cache file with this string
+ *    'user' => 'user', //user from xcache.admin.user settings
+ *    'password' => 'password', //plaintext password (xcache.admin.pass)
+ *  ));
  *
  * Memcache (http://memcached.org/)
  *
- * 	 Cache::config('default', array(
- *		'engine' => 'Memcache', //[required]
- *		'duration'=> 3600, //[optional]
- *		'probability'=> 100, //[optional]
- * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
- * 		'servers' => array(
- * 			'127.0.0.1:11211' // localhost, default port 11211
- * 		), //[optional]
- * 		'persistent' => true, // [optional] set this to false for non-persistent connections
- * 		'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
- *	));
+ *   Cache::config('default', array(
+ *    'engine' => 'Memcache', //[required]
+ *    'duration'=> 3600, //[optional]
+ *    'probability'=> 100, //[optional]
+ *    'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+ *    'servers' => array(
+ *      '127.0.0.1:11211' // localhost, default port 11211
+ *    ), //[optional]
+ *    'persistent' => true, // [optional] set this to false for non-persistent connections
+ *    'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
+ *  ));
  *
  *  Wincache (http://php.net/wincache)
  *
- * 	 Cache::config('default', array(
- *		'engine' => 'Wincache', //[required]
- *		'duration'=> 3600, //[optional]
- *		'probability'=> 100, //[optional]
- *		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
- *	));
+ *   Cache::config('default', array(
+ *    'engine' => 'Wincache', //[required]
+ *    'duration'=> 3600, //[optional]
+ *    'probability'=> 100, //[optional]
+ *    'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+ *  ));
  */
 Cache::config('default', array('engine' => 'File'));
 
@@ -129,3 +129,9 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
+CakePlugin::load('Opauth', array('routes' => true, 'bootstrap' => true));
+
+Configure::write('Opauth.Strategy.Facebook', array(
+   'app_id' => '388327344551040',
+   'app_secret' => 'c89c1b31312ec0d42a68e41d5db37c2d'
+));
